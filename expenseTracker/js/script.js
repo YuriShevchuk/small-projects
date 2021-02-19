98f3619e17fc34a9f3c57f4a0b45557f3c8103ae
YuriShevchuk <yurii.shev4uk@gmail.com>
2021-02-19 12:52:04 +0200

    display balanse, income & expense

--- expenseTracker/js/script.js
+++ expenseTracker/js/script.js
@@ -9,7 +9,7 @@ const dummyTransactions = [
 	{ id: 1, text: "Flower", amount: -20 },
 	{ id: 2, text: "Salary", amount: 300 },
 	{ id: 3, text: "Book", amount: -10 },
-	{ id: 4, text: "Camera", amount: -150 },
+	{ id: 4, text: "Camera", amount: 150 },
 ];
 
 let transactions = dummyTransactions;
@@ -18,19 +18,44 @@ let transactions = dummyTransactions;
 function addTransactionDOM(transaction) {
 	const sign = transaction.amount < 0 ? "-" : "+";
 
-    const item = document.createElement('li');
-    //add class based on value
-    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+	const item = document.createElement("li");
+	//add class based on value
+	item.classList.add(transaction.amount < 0 ? "minus" : "plus");
 
-    item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn">x</button>
+	item.innerHTML = `
+        ${transaction.text} <span>${sign}${Math.abs(
+		transaction.amount
+	)}</span> <button class="delete-btn">x</button>
     `;
-    list.appendChild(item);
+	list.appendChild(item);
+}
+
+// update the balance, income and expense
+function updateValues() {
+	const amounts = transactions.map((transaction) => transaction.amount);
+
+	const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+	const expense = (
+		amounts
+			.filter((item) => item < 0)
+			.reduce((acc, item) => (acc += item), 0) * -1
+	).toFixed(2);
+
+	const income = amounts
+		.filter((item) => item > 0)
+		.reduce((acc, item) => (acc += item), 0)
+		.toFixed(2);
+
+    balance.innerText = `$${total}`; 
+    money_plus.innerText = `$${income}`; 
+    money_minus.innerText = `$${expense}`; 
 }
 
 function init() {
-    list.innerHTML = '';
-    transactions.forEach(addTransactionDOM);
+	list.innerHTML = "";
+	transactions.forEach(addTransactionDOM);
+	updateValues();
 }
 
 init();
